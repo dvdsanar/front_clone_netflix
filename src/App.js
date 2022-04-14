cf82d53e9c449e94e912fad51fe73842cf59c511
David S <dvdsanar@gmail.com>
2022-04-14 10:13:12 +0200

    componente Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./componentes/Home/Home.js";
+import Header from "./componentes/Header/Header.js";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
