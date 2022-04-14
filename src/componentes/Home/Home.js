69aeef0d3dedb586b5436da37c296c47c27079a2
David S <dvdsanar@gmail.com>
2022-04-14 10:03:18 +0200

    componente Home

---
+++ src/componentes/Home/Home.js
@@ -0,0 +1,16 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home-contenedor">
+      <div>
+        <h1>NERFLIX</h1>
+      </div>
+      <div>
+        <img src="https://www.nintenderos.com/wp-content/uploads/2021/07/Netflix-Cropped.jpg"></img>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
