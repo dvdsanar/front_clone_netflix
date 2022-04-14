4a6321a370e2b366d18eeff630caf337087a24c1
David S <dvdsanar@gmail.com>
2022-04-14 10:33:44 +0200

    componente Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./componentes/Home/Home.js";
 import Header from "./componentes/Header/Header.js";
+import Footer from "./componentes/Footer/Footer.js";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/componentes/Footer/Footer.js
@@ -0,0 +1,15 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="enlacesFooter">
+        <a href="https://github.com/dvdsanar" target="_blank">
+          David
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
